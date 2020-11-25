900c17ecbe8d1aea6f5e2e1fad9faf1793108727
👨🏼‍🚀 🚀 Jeferson Dos Santos <dijeferson@gmail.com>
2020-11-25 02:38:08 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+function onLoad() {
+    const id = "avatar";
+    const baseName = "olivia_santos_memoji_";
+    const numberOfImages = 14;
+    var index = getRandomNumber(numberOfImages);
+
+    do {
+        index = getRandomNumber(numberOfImages);
+    } while (parseInt(getCookie("lastIndex"), 10) == index)
+
+    document.getElementById(id).src = "images/" + baseName + index + ".png";
+    setCookie("lastIndex", index, 1);
+}
+
+function getRandomNumber(top) {
+    return Math.floor(Math.random() * top)
+}
+
+function setCookie(cookie_name, cookie_value, expiry_days) {
+    var d = new Date();
+    d.setTime(d.getTime() + (expiry_days * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cookie_name + "=" + cookie_value + ";" + expires + ";path=/";
+}
+
+function getCookie(cookie_name) {
+    var name = cookie_name + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
